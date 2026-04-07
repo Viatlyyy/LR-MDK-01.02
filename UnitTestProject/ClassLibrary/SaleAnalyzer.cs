b16bc4e7dd2b18aa452b1fafb5fd199282ce5bf4
GavrishM <nanotechnology930@gmail.com>
2026-04-07 09:33:52 +0300

    переделать класс с подсчетом прибыли без загрузки

---
+++ UnitTestProject/ClassLibrary/SaleAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class SaleAnalyzer
+    {
+        public double CalculateRevenue(string productName)
+        {
+            Sales sales = new Sales();
+            List<Product> products = sales.LoadProduct();
+
+            foreach (Product product in products)
+            {
+                if (product.Name == productName)
+                {
+                    return product.Price * product.Count;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTestProject/TSaleAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TSaleAnalyzer
+    {
+        [TestMethod]
+        public void TestCalculateCalculateRevenue()
+        {
+            {
+                SaleAnalyzer analyzer = new SaleAnalyzer();
+                string productName = "Бананы";
+                double expectedRevenue = 300 * 3; 
+
+                
+                double actualRevenue = analyzer.CalculateRevenue(productName);
+
+                
+                Assert.AreEqual(expectedRevenue, actualRevenue);
+            }
+        }
+    }
+}
